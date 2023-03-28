fcbe994eb7c18587345daf62a7b5afba20ee91cc
azesm <azesm@yahoo.com>
2023-03-28 23:00:04 +0300

    16HomeWork not possible

--- 16HomeWork/index.php
+++ 16HomeWork/index.php
@@ -1,1 +1,18 @@
-<?php
+<?php error_reporting(-1);
+/*echo '<option>';
+$i = 1900;
+while ($i <= 2021) {
+    echo "<select border = "1" width = "100%">;
+    $i</br>
+    </select>";
+    $i++;
+    echo '<option>';
+}*/
+
+/*echo '<select>';
+$i = 1900;
+while ($i <= 2021) {
+    echo "<option>$i<br></option>";
+    $i++;
+    echo '</select>';
+}*/
