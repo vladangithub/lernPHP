605665fcf3774f142c03d48828a311c9945b92a4
azesm <azesm@yahoo.com>
2023-03-28 21:35:28 +0300

    16HomeWork

---
+++ 15While/index.php
@@ -0,0 +1,64 @@
+<?php error_reporting(-1);
+
+/*$i = 1;
+while ($i <= 20) {
+    echo "$i<br>";
+    $i++;
+}*/
+
+/*echo '<table border = "1" width = "100%">';
+$tr = 1;
+while ($tr <= 10) {
+    echo "<tr>";
+    $td = 1;
+    while ($td <= 20) {
+        echo "<td>" . $td * $tr . "</td>";
+        $td++;
+    }
+    echo "</tr>";
+    $tr++;
+}
+echo '</table>';*/
+
+/*$i = 1;
+while ($i <= 20) {
+    echo "$i<br>";
+    $i++;
+}*/
+
+$i = 11;
+do {
+    echo "$i<br>";
+    $i++;
+} while ($i <= 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
