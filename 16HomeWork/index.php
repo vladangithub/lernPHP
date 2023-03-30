84f7e88ce1df88ac315b6912579f4e3a2efe920a
azesm <azesm@yahoo.com>
2023-03-30 18:38:10 +0300

    end

--- 16HomeWork/index.php
+++ 16HomeWork/index.php
@@ -46,50 +46,51 @@ while ($year <= 2023) {
 echo '</select>';
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-echo '<table border = "1" width = "100%">';
+echo '<table border = "1px" width = "100%">';
 $tr = 1;
-while ($tr <= 10) {
+while ($tr < 10) {
     echo "<tr>";
     $td = 1;
-    while ($td <= 10) {
-        echo "<td>" . $td * $tr . "</td>";
+    while ($td < 10) {
+        echo "<td> $td * $tr = " . $td * $tr . "</td>";
         $td++;
     }
     echo "</tr>";
     $tr++;
 }
 echo '</table>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
