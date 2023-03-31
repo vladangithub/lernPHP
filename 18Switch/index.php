a0afb7275994992d6e3bece2846ce4a2b2f89d7b
azesm <azesm@yahoo.com>
2023-03-31 04:05:27 +0300

    18

---
+++ 18Switch/index.php
@@ -0,0 +1,114 @@
+<?php error_reporting(-1);
+
+
+
+/*if ($i == 1) {
+    echo 1;
+} elseif ($i == 2) {
+    echo 2;
+} elseif ($i == 3) {
+    echo 3;
+} elseif ($i == 4) {
+    echo 4;
+} else {
+    echo 5;
+};*/
+
+/*switch ($i) {
+    case 1:
+        echo 1;
+        break;
+    case 2:
+        echo 2;
+        break;
+    case 3:
+        echo 3;
+        break;
+    case 4:
+        echo 4;
+        break;
+    default:
+        echo 5;
+
+    case 5:
+        echo 4;
+        break;
+}*/
+
+
+/*$i = 200;
+
+switch ($i) {
+    case 1:
+
+    case 2:
+
+    case 3:
+    echo "Переменная < 4";
+    break;
+    case 4:
+        echo 4;
+        break;
+    default:
+        echo "Переменная > 4";
+
+}
+
+/*switch($beer)
+{
+    case 'tuborg';
+    case 'carlsberg';
+    case 'stella';
+    case 'heineken';
+        echo 'Хороший выбор';
+        break;
+    default;
+        echo 'Пожалуйста, сделайте новый выбор...';
+        break;
+}*/
+
+$i = "gray";
+
+switch ($i) {
+    case "red":
+        echo 'Stop';
+        break;
+    case "gray":
+        echo "Упал. отжался!";
+        break;
+    case "yellow":
+        echo "Attention";
+        break;
+    case "white":
+        echo "Подпрыгни!";
+        break;
+    default:
+        echo "RUN";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
