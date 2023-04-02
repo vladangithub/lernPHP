338879e692292909743453a3989c596a0369babb
azesm <azesm@yahoo.com>
2023-04-02 14:28:58 +0300

    arrayHalf

--- 21Array/index.php
+++ 21Array/index.php
@@ -1,1 +1,17 @@
-<?php
+<?php error_reporting(-1);
+
+$arr1 = array();
+/*$arr2 = [];
+
+var_dump($arr, $arr2);*/
+$arr1 = [
+    1 =>'Ivanov', 3 =>'Petrov', 'Siderov'
+];
+
+$arr1[7] = 'Xiderov';
+$arr1[] = 'Fiderov';
+$arr1[] = 10;
+$arr1[] = 1.2302;
+
+echo '<pre>' . print_r($arr1, 1) . '</pre>';
+echo $arr1[7];
